55f2374c99ea624275b00aae719338a41654baa1
simplymathematics <charliemeyers@gmail.com>
2018-12-07 17:01:33 -0500

    updated internet, hdi

--- poverty/hdi.R
+++ poverty/hdi.R
@@ -1,30 +1,19 @@
 # HUMAN DEVELOPMENT INDEX (HDI)
 
-suppressWarnings(library(rvest, quietly =TRUE))
-suppressWarnings(library(dplyr, quietly =TRUE))
-suppressWarnings(library(stringr, quietly =TRUE))
-suppressWarnings(library(tidyr, quietly =TRUE))
-suppressWarnings(library(dplyr, quietly =TRUE))
-suppressWarnings(library(ggplot2, quietly =TRUE))
-suppressWarnings(library(curl, quietly =TRUE))
+library(curl)
+library(XML)
 
 try(setwd("poverty/"))
 
-## Downloading HDI data from official sources includes javascript requirements, which are outside the scope of this class. 
-## For your reference, you can unhash and run code below to view .html output. 
-
-#### curl_download("http://hdr.undp.org/en/indicators/137506", "hdi.html")
-#### hdi.html <- read_html("hdi.html")
-
-## For the purposes of this assignment, we have instead chosen to save and import the .csv file directly from the UNDP. 
-
-
-## HDI 2007 - 2017
-hdi <- read.csv("hdi.csv", header = FALSE) 
-hdi %>% 
-  select(-V3:-V36, -V38, -V40, -V42, -V44, -V46, -V48, -V50, -V52, -V54, -V56, -V58) %>%
-  rename(hdi.rank = V1, country = V2, yr.2007 = V37, yr.2008 = V39, yr.2009 = V41, yr.2009 = V41, yr.2010 = V43, yr.2011 = V45, yr.2012 = V47, yr.2013 = V49, yr.2014 = V51, yr.2015 = V53, yr.2016 = V55, yr.2017 = V57) %>%
-  slice(3:191) %>%
-  mutate(hdi.rank = as.numeric(hdi.rank)) %>%
-  arrange(as.numeric(hdi.rank))
-
+poverty.index <- curl_download("http://api.worldbank.org/v2/countries/all/indicators/UNDP.HDI.XD?date=2000:2017", "poverty.xml");
+xmlParse("poverty.xml");
+xml.data <- xmlParse("poverty.xml");
+rootNode <- xmlRoot(xml.data)
+xml.data <- xmlSApply(rootNode,function(x) xmlSApply(x, xmlValue))
+xml.frame <- data.frame(t(xml.data),row.names=NULL)
+hdi <- xml.frame
+annual.hdi <- c()
+for (year in 2000:2017){
+  annual.hdi[[year-1999]] <- subset(hdi, date==year)
+}
+SSS
